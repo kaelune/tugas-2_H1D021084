b5395e18e22e48d90e68517fe79593ed7c7801e2
Kaelune <pilarhadi2004@gmail.com>
2023-09-20 02:31:25 +0700

    Update tampil_data.dart

--- lib/ui/form_data.dart
+++ lib/ui/form_data.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:tugas_2/ui/tampil_data.dart';
 
 class FormData extends StatefulWidget {
   const FormData({Key? key}) : super(key: key);
@@ -9,12 +10,66 @@ class FormData extends StatefulWidget {
 }
 
 class _FormDataState extends State<FormData> {
+  final _namaController = TextEditingController();
+  final _nimController = TextEditingController();
+  final _tahunLahirController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Input"),
       ),
+      body: Container(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            _textboxNama(),
+            _textboxNIM(),
+            _textboxTahunLahir(),
+            _tombolSimpan()
+          ],
+        ),
+      )
     );
+  }
+
+  _textboxNama() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "Nama"),
+      controller: _namaController,
+    );
+  }
+
+  _textboxNIM() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "NIM"),
+      controller: _nimController,
+    );
+  }
+
+  _textboxTahunLahir() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "Tahu_textboxtahunLahir"),
+      controller: _tahunLahirController,
+    );
+  }
+
+
+  _tombolSimpan(){
+    return ElevatedButton(
+      onPressed: (){
+        String nama = _namaController.text;
+        String nim = _nimController.text;
+        int tahunLahir = int.parse(_tahunLahirController.text);
+        Navigator.of(context as BuildContext).push(MaterialPageRoute(
+          builder: (context) => TampilData(
+            nama: nama,
+            nim: nim,
+            tahunLahir: tahunLahir,
+          )
+        ));
+      },
+      
+    child: const Text('Simpan Data'),);
   }
 }
--- lib/ui/tampil_data.dart
+++ lib/ui/tampil_data.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:tugas_2/ui/form_data.dart';
 
-class TampilData extends StatelessWidget {
-  final String data; //contoh data yang di-passing
-  
-  const TampilData({Key? key, required this.data}) : super(key: key);
+class TampilData extends StatelessWidget { //contoh data yang di-passing
 
+  const TampilData({Key? key, required int this.tahunLahir, required String this.nim, required String this.nama}) : super(key: key);
+  final String nama;
+  final String nim;
+  final int tahunLahir;
   @override
   Widget build(BuildContext context) {
+    var usia = DateTime.now().year-tahunLahir;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Perkenalan"),
       ),
+      body: Container(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Text("Nama saya $nama, dengan NIM $nim dan berusia $usia"),
+          ],
+        ),
+      )
     );
   }
 }
